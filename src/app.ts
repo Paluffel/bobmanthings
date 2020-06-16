fa4c9f575eb702e476f56455f387f14c2b7376cb
Paluffel <40839103+Paluffel@users.noreply.github.com>
2020-06-16 23:50:24 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -44,12 +44,58 @@ export default class HelloWorld {
                     },
                     transform: {local: {
                         position: { x: 0.0003, y: -0.0503 , z: -0.047365 },
-                        scale: { x: 1.007, y: 1.007, z: 1.007},
+                        scale: { x: 1.005, y: 1.005, z: 1.005},
                        
                     }}
                 }
             });
 
+	// Create cube.
+    console.log(`User joined: ${user.name}`);
+    console.log(user);
+    if (user.name === "BOBWORKS"){
+        const mirror =   Actor.CreateFromLibrary(this.context, {
+    resourceId: "artifact: 1314062242190197175",
+    actor: {
+        name: 'Mirror',
+        grabbable: true,
+        exclusiveToUser: user.id,
+        attachment: {
+            userId: user.id,
+            attachPoint: 'right-hand'
+        },
+        transform: {local: {
+            position: { x: 0, y: 0, z:0  },
+            scale: { x: 0.2, y: 0.2, z: 0.2}
+        }}
     }
+});
+    //te button behavior for cube.
+mirror.setBehavior(ButtonBehavior).onButton("pressed", (user: User) => {
+    if (!this.attachedItems[user.id]) {
+// If item for user does not exist, create it and add to list.
+this.attachedItems[user.id] = Actor.CreateFromLibrary(this.context, {
+    resourceId: "artifact: 1496629452858196474",
+    actor: {
+        name: 'Retro',
+        attachment: {
+            userId: user.id,
+            attachPoint: 'head'
+        },
+        transform: {local: {
+            position: { x: 0.0003, y: -0.0503 , z: -0.047365 },
+            scale: { x: 1.007, y: 1.007, z: 1.007},
+        }}
+    }
+});
+
+   } else {
+    // If item already exists, destroy it and delete from list.
+    this.attachedItems[user.id].destroy();
+    delete this.attachedItems[user.id];
+}
+});
+    }
+}
 }
 }
